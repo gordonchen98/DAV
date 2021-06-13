bf4b4cc176ee74115e49a203f622b9a55defd99a
Gordon Chen <gordon890908@gmail.com>
2021-06-13 23:11:09 +0800

    Change the truncation of the product

--- FFT/butterfly_design.sv
+++ FFT/butterfly_design.sv
@@ -10,11 +10,17 @@ module butterfly_design(a, b, w, out1, out2);
 	wire [15:0] realW = w[31:16];
 	wire [15:0] imagW = w[15:0];
 	wire [15:0] realResult, imagResult;
+	wire [31:0] realResult1, realResult2, imagResult1, imagResult2;
+	
+	assign  realResult1 = realB*realW;
+	assign  realResult2 = imagB*imagW;
+	assign  imagResult1 = realB*imagW;
+	assign  imagResult2 = imagB*realW;
 	
 	// compute w*b
-	assign realResult = realB*realW - imagB*imagW;
-	assign imagResult = realB*imagW + imagB*realW;
-	
+	assign realResult =  realResult1[30:15] - realResult2[30:15];
+	assign imagResult =  imagResult1[30:15] - imagResult2[30:15];
+
 	// find the outputs
 	assign out1 = {(realA + realResult), (imagA + imagResult)};
 	assign out2 = {(realA - realResult), (imagA - imagResult)};
